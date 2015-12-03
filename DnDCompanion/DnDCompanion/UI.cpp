c606c3fa3b06448db8e0e8240bf2de670c24b26c
Cody Brewster <ctbrewster@tamu.edu>
2015-12-02 22:44:58 -0600

    Update UI.cpp

--- DnDCompanion/DnDCompanion/UI.cpp
+++ DnDCompanion/DnDCompanion/UI.cpp
@@ -1,14 +1,14 @@
 // Installing Freeglut
 /*
-	Open your solution, click TOOLS->NuGet Package Manager->Package Manager Console to open a NuGet console, type Install-Package freeglut.
-	--
-	For VS 2013, use nupengl.core package instead.
+Open your solution, click TOOLS->NuGet Package Manager->Package Manager Console to open a NuGet console, type Install-Package freeglut.
+--
+For VS 2013, use nupengl.core package instead.
 */
 
 // Installing glui
 /*
-	https://masdel.wordpress.com/2010/06/13/installing-glui-using-vc-on-windows/
-	If you have problems, read the note at the end of the post
+https://masdel.wordpress.com/2010/06/13/installing-glui-using-vc-on-windows/
+If you have problems, read the note at the end of the post
 */
 
 #include <string.h>
@@ -37,11 +37,14 @@ int right_y;				//Y-Coordinate where right mouse button was down
 int entityID;				//Default entity ID assigned the new entities then increamented
 int draw_mode;				//If 0 draw entity. If 1 draw terrain.
 TerrainType terrain_type;	//Type of terrain being drawn
+EntityType entity_type;		//Type of Entity selected
 
 //GLUI Stuff
 GLUI_RadioGroup *terrain_radio;
+GLUI_RadioGroup *entity_radio;
 GLUI_RadioGroup *mode_radio;
 int tt = 0;
+int et = 0;
 
 float findxMax(int x){
 
@@ -122,12 +125,38 @@ void drawEntity(Entity *E){
 	float yMax = findyMax(yin);
 	float yMin = findyMin(yin);
 
-	glColor3f(.2, .5, .2);
+	switch (E->getType()){
+	case EntityType::Tank:
+		glColor3f(.1, .1, .2);
+		break;
+	case EntityType::Melee:
+		glColor3f(.1, .1, .3);
+		break;
+	case EntityType::Ranged:
+		glColor3f(.1, .1, .4);
+		break;
+	case EntityType::Magic:
+		glColor3f(.1, .1, .5);
+		break;
+	case EntityType::Healer:
+		glColor3f(.1, .1, .6);
+		break;
+	case EntityType::Mob:
+		glColor3f(.1, .1, .7);
+		break;
+	case EntityType::Boss:
+		glColor3f(.1, .1, .8);
+		break;
+	case EntityType::Neutral:
+		glColor3f(.1, .1, .9);
+		break;
+	}
+	//glColor3f(.2, .5, .2);
 	glBegin(GL_POLYGON);
-	glVertex2f(xMin, yMin);
-	glVertex2f(xMax, yMin);
-	glVertex2f(xMax, yMax);
-	glVertex2f(xMin, yMax);
+	glVertex2f(xMin + .01, yMin + .02);
+	glVertex2f(xMax - .01, yMin + .02);
+	glVertex2f(xMax - .01, yMax - .02);
+	glVertex2f(xMin + .01, yMax - .02);
 	glEnd();
 	//glFlush();
 }
@@ -142,15 +171,15 @@ void myDisplay(void){
 	//Draw Terrain
 	for (int i = bottom_left_x; i < bottom_left_x + display_x; i++){
 		for (int j = bottom_left_y; j < bottom_left_y + display_y; j++){
-			drawTerrain(map->terrainAtSquare(i, j), i , j);
+			drawTerrain(map->terrainAtSquare(i, j), i, j);
 		}
 	}
 
 	//Draw Entities
 	for (int i = bottom_left_x; i < bottom_left_x + display_x; i++){
 		for (int j = bottom_left_x; j < bottom_left_y + display_y; j++){
-			for (int k = 0; k < map->entitiesAtSquare(i,j).size(); k++){
-				drawEntity(map->entitiesAtSquare(i,j)[k]);
+			for (int k = 0; k < map->entitiesAtSquare(i, j).size(); k++){
+				drawEntity(map->entitiesAtSquare(i, j)[k]);
 			}
 		}
 	}
@@ -161,7 +190,7 @@ void myDisplay(void){
 		glBegin(GL_LINES);
 		for (int i = 0; i <= display_x; i++){
 			glVertex2f((9.0f / 16.0) * (2 * i * (1.0f / display_x)) - 1, -1.0f);
-			glVertex2f((9.0f / 16.0) * (2 * i * (1.0f / display_x)) - 1,  1.0f);
+			glVertex2f((9.0f / 16.0) * (2 * i * (1.0f / display_x)) - 1, 1.0f);
 		}
 		for (int i = 0; i <= display_y; i++){
 			glVertex2f(-1.0f, (2 * i * (1.0f / display_y)) - 1);
@@ -196,19 +225,19 @@ void myKeyboard(unsigned char Key, int x, int y){
 		if (bottom_left_y > 0)
 			bottom_left_y--;
 		break;
-	/*case 9:		//tab
+		/*case 9:		//tab
 		if (terrain_type == TerrainType::Basic)
-			terrain_type = TerrainType::River;
+		terrain_type = TerrainType::River;
 		else
-			terrain_type = TerrainType::Basic;
+		terrain_type = TerrainType::Basic;
 		break;
-	case 32:	//space
+		case 32:	//space
 		if (draw_mode == 0)
-			draw_mode = 1;
+		draw_mode = 1;
 		else
-			draw_mode = 0;
+		draw_mode = 0;
 		break;*/
-	default:	
+	default:
 		break;
 	}
 	myDisplay();
@@ -226,10 +255,10 @@ void myMouse(int button, int button_state, int x, int y){
 				case 0:
 					x_pos = display_x * x / 720;
 					y_pos = display_y * (720 - y) / 720;
-					map->createEntity(entityID, Player, x_pos + bottom_left_x, y_pos + bottom_left_y, "Player", "", 10, 30);
+					map->createEntity(entityID, (EntityType)et, x_pos + bottom_left_x, y_pos + bottom_left_y, "Player", "", 10, 30);
 					entityID++;
 					break;
-				/*case 1:
+					/*case 1:
 					x_pos = display_x * x / 720;
 					y_pos = display_y * (720 - y) / 720;
 					map->updateTerrainAtSquare(terrain_type, x_pos + bottom_left_x, y_pos + bottom_left_y);
@@ -278,7 +307,7 @@ void myMotion(int x, int y){
 	if (left_mouse_down && (draw_mode == 1) && (x < 720)){
 		x_pos = display_x * x / 720;
 		y_pos = display_y * (720 - y) / 720;
-		map->updateTerrainAtSquare((TerrainType) tt, x_pos + bottom_left_x, y_pos + bottom_left_y);
+		map->updateTerrainAtSquare((TerrainType)tt, x_pos + bottom_left_x, y_pos + bottom_left_y);
 	}
 	myDisplay();
 }
@@ -327,6 +356,17 @@ int main(int argc, char **argv)
 	glui->add_radiobutton_to_group(terrain_radio, "Brush");
 	glui->add_radiobutton_to_group(terrain_radio, "Desert");
 	glui->add_radiobutton_to_group(terrain_radio, "Custom");
+
+	GLUI_Panel *entity_panel = new GLUI_Panel(glui, "Entity Type");
+	entity_radio = glui->add_radiogroup_to_panel(entity_panel, &et);
+	glui->add_radiobutton_to_group(entity_radio, "Tank");
+	glui->add_radiobutton_to_group(entity_radio, "Melee");
+	glui->add_radiobutton_to_group(entity_radio, "Ranged");
+	glui->add_radiobutton_to_group(entity_radio, "Magic");
+	glui->add_radiobutton_to_group(entity_radio, "Healer");
+	glui->add_radiobutton_to_group(entity_radio, "Mob");
+	glui->add_radiobutton_to_group(entity_radio, "Boss");
+	glui->add_radiobutton_to_group(entity_radio, "Neutral");
 
 	glutSetWindow(main_window);
 	glui->set_main_gfx_window(main_window);

7b5aa1e257ab8f8550fb6bd8a38a15b4b8d1e486
Cody Brewster <ctbrewster@tamu.edu>
2015-12-03 22:01:29 -0600

    Null click bug fix

--- DnDCompanion/DnDCompanion/Entity.h
+++ DnDCompanion/DnDCompanion/Entity.h
@@ -2,7 +2,7 @@
 #include <string>
 #include <tuple>
 
-enum EntityType { Tank, Melee, Ranged, Magic, Healer, Mob, Boss, Neutral };
+enum EntityType { Tank, Melee, Ranged, Magic, Healer, Mob, Boss, Neutral, Null };
 
 class Entity
 {
